22fc20dfc9f0c000b6a2ab2a61e5298384ca17cb
Romaon <52658946+romainvg@users.noreply.github.com>
2019-09-02 17:50:06 +0200

    Update IP Scan

--- IP Scan.py
+++ IP Scan.py
@@ -1,6 +1,4 @@
 import sys
-import os
-import os.path
 from datetime import datetime
 from scapy.all import srp, Ether, ARP, conf
 
@@ -35,70 +33,8 @@ def arp():
 
     return devices
 
-
-def createText(deviceList):
-    devices = open("devices.txt", "w")
-    for mac, ip in deviceList.iteritems():
-        devices.write(mac + "&" + ip + "\n")
-    devices.close()
-
-
-def textList():
-    l = open("devices.txt", "r")
-    List = l.readlines()
-    textList = {}
-    for i in range(len(List)):
-        mac, ip = List[i].split("&")
-        ip = ip[:-1]
-        textList[mac] = ip
-
-    l.close()
-    return textList
-
-
-def checkText():
-    newText = {}
-    oldText = {}
-    newText = arp()
-    oldText = textList()
-    lastMacs = list(oldText.keys())
-    newMacs = list(newText.keys())
-    text = {}
-
-    for i in range(len(newText)):
-        if newMacs[i] in lastMacs:
-            if (newText[newMacs[i]] == oldText[newMacs[i]]):
-                text[newMacs[i]] = newText[newMacs[i]]
-                continue
-            else:
-                while (1):
-                    option = input("\n[*] IP of The MAC address %s that is in your lan has changed. Do you want save this change ? (y/n): " %newMacs[i])
-                    if (option == "y" or option == "Y"):
-                        text[newMacs[i]] = newText[newMacs[i]]
-                        break
-                    elif (option == "n" or option == "N"):
-                        text[newMacs[i]] = oldText[newMacs[i]]
-                        break
-                    else:
-                        print('[*] Please Only Select "Y" or "N" Option!')
-        else:
-            while (1):
-                option = input("\n[*] Do you want save this device: %s to list ? (y/n): " % newMacs[i])
-                if (option == "y" or option == "Y"):
-                    text[newMacs[i]] = newText[newMacs[i]]
-                    break
-                elif (option == "n" or option == "N"):
-                    break
-                else:
-                    print('[*] Please Select Only "Y" or "N" Option !')
-    createText(text)
-
-
 def main():
-    if os.path.exists("devices.txt"):
-        checkText()
-    else:
-        createText(arp())
 
+    arp()
 
 main()
