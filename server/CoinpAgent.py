2b0355a888c600d3fb98cfe616b718cd3690564a
Romaon <romain.guihot@gmail.com>
2019-09-30 11:43:58 +0200

    Update CoinpAgent.py

--- server/CoinpAgent.py
+++ server/CoinpAgent.py
@@ -2,6 +2,13 @@
 #                                                                               #
 #  CoinpAgent.py                                                                #
 #                                                                               #
+#  The software is licensed Creative Commons CC-BY-NC-SA. Under this agreement  #
+#  you are authorized to use, share on the same rights or edit this software    #
+#  for personnal purpose only. You are not allow to sell this software.         #
+#                                                                               #
+#    Official Website : https://coinpaign.com                                   #
+#    Contact : romain.guihot@gmail.com
+
 #  This module is installed in computers to be managed by Remote Administrator. #
 #  Function:                                                                    #
 #    1. Send some of computer-informations to Remote Administrator.             #
