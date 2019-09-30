1b1edd395a571f04c40c55bcf14d3713f1460640
Romaon <romain.guihot@gmail.com>
2019-09-30 11:32:10 +0200

    Update Port_Scan.py

--- Port_Scan.py
+++ Port_Scan.py
@@ -2,12 +2,12 @@
 #                                                                               #
 #  Port_Scan.py                                                                 #
 #                                                                               #                                                                           #
-#   The software is licensed Creative Commons CC-BY-NC-SA. Under this agreement #
-#   you are authorized to use, share on the same rights or edit this software   #
-#   for personnal purpose only. You are not allow to sell this software.        #
+#  The software is licensed Creative Commons CC-BY-NC-SA. Under this agreement #
+#  you are authorized to use, share on the same rights or edit this software   #
+#  for personnal purpose only. You are not allow to sell this software.        #
 #                                                                               #
-#    Official Website : https://coinpaign.com                                   #
-#    Contact : romain.guihot@gmail.com                                          #
+#  Official Website : https://coinpaign.com                                   #
+#  Contact : romain.guihot@gmail.com                                          #
 
 #  This module is installed in Remote Administrator' computer.                  #
 #  This is used in RemoteAdminTool.py.                                          #
