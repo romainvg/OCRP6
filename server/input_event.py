3e67036a350b8d530920d94ac7448fcbe26ebbfd
Romaon <romain.guihot@gmail.com>
2019-09-30 11:49:05 +0200

    Update input_event.py

--- server/input_event.py
+++ server/input_event.py
@@ -2,6 +2,13 @@
 #                                                                               #
 #  input_event.py                                                               #
 #                                                                               #
+#  The software is licensed Creative Commons CC-BY-NC-SA. Under this agreement  #
+#  you are authorized to use, share on the same rights or edit this software    #
+#  for personnal purpose only. You are not allow to sell this software.         #
+#                                                                               #
+#    Official Website : https://coinpaign.com                                   #
+#    Contact : romain.guihot@gmail.com                                          #
+
 #  This module is installed in computers to be managed by Remote Administrator. #
 #  This is used in RDC_Server.py                                                # 
 #  Function:                                                                    #
